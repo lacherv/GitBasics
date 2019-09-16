c07336bb1d685d1825a5894accb440a7c107cc3c
Christian Awelakoue <awelakoue@gmail.com>
2019-09-16 14:54:20 +0200

    111

--- main.js
+++ main.js
@@ -1,7 +1,33 @@
 /************ Const & Let **************** */
+const name = 'Chris';
+const person = {
+    name: 'chris',
+    title: 'Me'
+};
+person.name = 'kamo';
+// console.log(person.name);
+
+const nums = ['1', '3', '4', '5'];
+nums.push('6');
+// console.log(nums);
+let test = '132';
+test = 23;
+console.log(test);
 
 /************ Arrow Functions **************** */
+// function sayHello() {
+//     console.log(' Hello ' + name);
+// }
+const sayHello = () => console.log(`Hello ${name}`);
+sayHello();
 
+// function sayHello1(name1) {
+//     console.log(' Hello ' + name1);
+// }
+const sayHello1 = name1 => console.log(`Hello ${name1}`);
+sayHello1('aaron');
+const sum = (a, b) => console.log(`Sum of ${a} and ${b} equal ${a + b}`);
+sum(2, 4);
 /************ Map **************** */
 
 /************ Filter **************** */
